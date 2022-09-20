019ef983ff1a8700bb9fb301dfe20c6ee9ce1881
Eli Sailer-Haugland <eli092011@gmail.com>
2022-09-20 09:35:34 -0500

    init

---
+++ pages/api/email.js
@@ -0,0 +1,3 @@
+export default function email(){
+    return 200
+}
---
+++ pages/hello.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export default function Hello() {
+    const [name,setName] = useState()
+    async function Submit(){
+        const res =await fetch('/api/hello',{
+            method:"POST",
+            body: null
+        }).then(t=> t.json())
+        setName(res.name)
+    }
+    return (<div>
+        {name}
+        <form method="POST" action="/api/hello">
+            <input type="button" value="Submit" onClick={Submit}/>
+        </form>
+        </div>
+    )
+}
